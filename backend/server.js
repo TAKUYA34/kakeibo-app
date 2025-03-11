a6bd2ba8c6b42eac141d58a3a8f8d25700ce55cd
takuya-Y <hakenad907@gmail.com>
2025-03-11 19:10:18 +0900

    mongoDB接続エラーが出ていたため、修正した

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const port = 5001;
 
-app.get('/api', (req, res) => {
+// MONGO_URIをログに出力する
+const MONGO_URI = "mongodb://mongo:27017/mern"
+console.log("Connecting to MongoDB at:", MONGO_URI);
+
+// Connect to MongoDB
+mongoose.connect(MONGO_URI)
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.log('Cannnot connect to MongoDB:', err));
+
+app.get('/', (req, res) => {
   res.send('API is working');
 });
 
