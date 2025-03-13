e3723f0cd086e28a677ce587f9a05f0a4a23b871
takuya-Y <hakenad907@gmail.com>
2025-03-13 20:30:46 +0900

    css modulesに変更

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,5 +1,6 @@
 import { useAuth } from "../context/AuthContext";
 import { useNavigate } from "react-router-dom";
+import '../css/Dashboard.css';
 
 const Dashboard = () => {
   const { user, logout } = useAuth();
