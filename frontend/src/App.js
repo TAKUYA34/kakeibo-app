e7a70113a29dd3ec25b74c21bb0bb7352602fc07
takuya-Y <hakenad907@gmail.com>
2025-03-20 12:26:47 +0900

    main機能引越し

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
-import Register from "./components/Register";
-import Login from "./components/Login";
-import Dashboard from "./components/Dashboard";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import Home from "./pages/Home.js";
 
 const App = () => {
   return (
     <AuthProvider>
       <Router>
         <Routes>
-          <Route path="/" element={<Dashboard />} />
+          <Route path="/" element={<Home />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/dashboard" element={<Home />} />
         </Routes>
       </Router>
     </AuthProvider>
