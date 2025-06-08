27b8aed10806a396dc68f5b27f9a213edb71f795
takuya-Y <hakenad907@gmail.com>
2025-06-08 22:25:18 +0900

    引越し／削除

--- frontend/src/components/LoginButton.js
+++
@@ -1,9 +0,0 @@
-import { useAuth } from "../context/AuthContext";
-
-const LogoutButton = () => {
-  const { logout } = useAuth();
-
-  return <button onClick={logout}>ログアウト</button>;
-};
-
-export default LogoutButton;
