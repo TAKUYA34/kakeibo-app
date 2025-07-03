494ffba9033c81ff1475d2847abade63b7f34c36
takuya-Y <hakenad907@gmail.com>
2025-07-03 13:05:25 +0900

    スキーマ修正

--- backend/models/Transaction.js
+++ backend/models/Transaction.js
@@ -4,13 +4,16 @@ const mongoose = require('mongoose');
 const transactionSchema = new mongoose.Schema({
   transaction_id: { type: String, unique: true },
   user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },       // ユーザーとの紐付け
-  category_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true },
+  // category_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true },
   trans_type: { type: String, enum: ['income', 'expense'], required: true }, // 収入/支出
-  amount: { type: Number, required: true },
-  total_amount: { type: Number, required: true },
-  cost_type: {type: String, enum: ['fixed', 'variable'], required: true }, // 固定/変動
-  memo: { type: String },
-  trans_date: { type: Date, required: true },
+  amount: { type: Number, required: true }, // 金額
+  total_amount: { type: Number, required: true }, // 合計金額
+  memo: { type: String }, // メモ
+  // cost_type: {type: String, enum: ['fixed', 'variable'], required: true }, // 固定/変動
+  major_sel: { type: String, required: true }, // 大項目
+  middle_sel: { type: String, required: true }, // 中項目
+  minor_sel: { type: String, required: true }, // 小項目
+  // trans_date: { type: Date, required: true },
   created_at: { type: Date, default: Date.now },
   updated_at: { type: Date, default: Date.now }
 });
