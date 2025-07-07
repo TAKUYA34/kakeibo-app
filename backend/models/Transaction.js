5bf74ea1b67fa19faae22cf1bae670ec17800dc2
takuya-Y <hakenad907@gmail.com>
2025-07-07 12:48:33 +0900

    小項目は空欄でも登録可に変更した

--- backend/models/Transaction.js
+++ backend/models/Transaction.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 // transactionsテーブルのスキーマ定義
 const transactionSchema = new mongoose.Schema({
   transaction_id: { type: String, unique: true },
-  user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },       // ユーザーとの紐付け
+  user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, // ユーザーとの紐付け
   // category_id: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true },
   trans_type: { type: String, enum: ['income', 'expense'], required: true }, // 収入/支出
   amount: { type: Number, required: true }, // 金額
@@ -12,7 +12,7 @@ const transactionSchema = new mongoose.Schema({
   // cost_type: {type: String, enum: ['fixed', 'variable'], required: true }, // 固定/変動
   major_sel: { type: String, required: true }, // 大項目
   middle_sel: { type: String, required: true }, // 中項目
-  minor_sel: { type: String, required: true }, // 小項目
+  minor_sel: { type: String, }, // 小項目
   // trans_date: { type: Date, required: true },
   created_at: { type: Date, default: Date.now },
   updated_at: { type: Date, default: Date.now }
