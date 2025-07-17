7f9d5bd9a7b31c9f8fd1d38347abbf44d8b92fdb
takuya-Y <hakenad907@gmail.com>
2025-07-17 14:38:51 +0900

    /adminにアクセス時はadminページにアクセスするように分離した

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App';// メインのAppコンポーネント
+import AdminApp from './AdminApp'; // 管理者用のAppコンポーネント
 import reportWebVitals from './reportWebVitals';
 import "./global.css"; // インポート
 
+// 管理者フラグ
+const isAdminPath = window.location.pathname.startsWith('/admin');
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+root.render(isAdminPath ? (
+  <React.StrictMode>
+    <AdminApp />
+  </React.StrictMode>
+) : (
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
