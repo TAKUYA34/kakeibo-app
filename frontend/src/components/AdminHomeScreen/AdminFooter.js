3743288ea04a83cdb519f9737c8e7e5715c3a83a
takuya-Y <hakenad907@gmail.com>
2025-07-18 15:14:31 +0900

    AdminFooter画面を作成した

---
+++ frontend/src/components/AdminHomeScreen/AdminFooter.js
@@ -0,0 +1,18 @@
+import styles from "../../styles/AdminHomeStatic/AdminFooter.module.css";
+
+const AdminFooter = () => {
+  return (
+    <footer>
+      <div className={styles.footerContainer}>
+        <p>&copy; Copyright All Rights Reserved. Kakeibo-App {new Date().getFullYear()}.</p>
+        <p className={styles.adminFooterMenu}>
+          管理者向けページ | <a href='/' target='_self'>ヘルプ</a> | 
+          <a href='/' target='_self'>システム設定</a>
+        </p>
+        <p className="mt-1 text-gray-400">Ver. 1.0.0 | Last update: {`${new Date().getFullYear()}-${new Date().getMonth() + 1}-${new Date().getDate()}`}</p>
+      </div>
+    </footer>
+  );
+}
+
+export default AdminFooter;
