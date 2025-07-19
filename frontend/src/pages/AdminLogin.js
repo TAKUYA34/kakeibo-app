27c515db1719cb64c274c5fce49411f5afea7166
takuya-Y <hakenad907@gmail.com>
2025-07-19 16:52:43 +0900

    AdminLogin画面にHeader不要

--- frontend/src/pages/AdminLogin.js
+++ frontend/src/pages/AdminLogin.js
@@ -1,11 +1,9 @@
 // pages/AdminLogin.js
-import AdminHeader from "../components/AdminHomeScreen/AdminHeader";
 import AdminLoginPage from "../components/RegisterScreen/AdminLoginForm";
 
 const AdminLogin = () => {
   return (
     <>
-      <AdminHeader />
       <AdminLoginPage />
     </>
   );
