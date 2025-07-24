91625432a717743f8f918e99da6eabdd100a1c89
takuya-Y <hakenad907@gmail.com>
2025-07-24 18:31:27 +0900

    無駄な処理を削除

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,13 +1,10 @@
 // pages/Login.js
-
-import Header from "../components/HomeScreen/Header";
 import LoginPage from "../components/RegisterScreen/LoginForm";
 import Footer from "../components/HomeScreen/Footer";
 
 const Login = () => {
   return (
     <>
-      <Header />
       <LoginPage />
       <Footer />
     </>
--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,13 +1,10 @@
 // pages/Register.js
-
-import Header from "../components/HomeScreen/Header";
 import SignUpForm from "../components/RegisterScreen/SignUpForm";
 import Footer from "../components/HomeScreen/Footer";
 
 const Register = () => {
   return (
     <>
-      <Header />
       <SignUpForm />
       <Footer />
     </>
