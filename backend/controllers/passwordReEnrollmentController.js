1c3b720042cadd19d35d2cc35c2a79dc424cb110
takuya-Y <hakenad907@gmail.com>
2025-07-29 17:45:12 +0900

    修正

--- backend/controllers/passwordReEnrollmentController.js
+++ backend/controllers/passwordReEnrollmentController.js
@@ -1,5 +1,6 @@
 const passwordReEnrollmentService = require('../services/passwordReEnrollmentService');
 
+/* 新しいパスワードに変更する */
 const resetPassword = async (req, res) => {
   const { token, newPassword } = req.body;
 
@@ -7,7 +8,7 @@ const resetPassword = async (req, res) => {
     await passwordReEnrollmentService.fetchResetPassword(token, newPassword);
     res.status(200).json({ message: 'パスワードがリセットされました。' });
   } catch (error) {
-    console.error('リセット失敗:', error);
+    // console.error('リセット失敗:', error);
     res.status(400).json({ message: error.message || 'パスワードリセットに失敗しました。' });
   }
 };
--- backend/services/passwordReEnrollmentService.js
+++ backend/services/passwordReEnrollmentService.js
@@ -3,22 +3,27 @@ const bcrypt = require('bcrypt');
 
 /* 新しいパスワードをハッシュ化して再登録する */
 const fetchResetPassword = async (token, newPassword) => {
-  //照合
-  const user = await passwordReEnrollmentRepository.findByResetToken(token);
 
-  if (!user) {
-    throw new Error('無効なトークンです。');
+  try {
+    //照合
+    const user = await passwordReEnrollmentRepository.findByResetToken(token);
+
+    if (!user) {
+      throw new Error('無効なトークンです。');
+    }
+    // ハッシュ化
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+
+    // パスワードとトークンを更新
+    user.password = hashedPassword;
+    user.resetPasswordToken = null;
+    user.resetPasswordExpires = null;
+    
+    // 登録する
+    await user.save();
+  } catch (err) {
+    throw err;
   }
-  // ハッシュ化
-  const hashedPassword = await bcrypt.hash(newPassword, 10);
-
-  // パスワードとトークンを更新
-  user.password = hashedPassword;
-  user.resetPasswordToken = null;
-  user.resetPasswordExpires = null;
-  
-  // 登録する
-  await user.save();
 };
 
 module.exports = {
