8fd1184f8ec07ea4bc587f036d64a0625327d1b0
takuya-Y <hakenad907@gmail.com>
2025-07-29 15:01:00 +0900

    修正

--- backend/services/requestPasswordResetService.js
+++ backend/services/requestPasswordResetService.js
@@ -4,18 +4,22 @@ const sendResetEmail = require('../utils/mailer');
 
 /* 指定したユーザーのメールチェック */
 const handlePasswordResetRequest = async (email) => {
-  const user = await requestPasswordResetRepository.findByEmail(email);
-  if (!user) throw new Error('ユーザーが見つかりません');
+  try {
+    const user = await requestPasswordResetRepository.findByEmail(email);
+    if (!user) throw new Error('ユーザーが見つかりません');
 
-  // トークン生成
-  const token = crypto.randomBytes(32).toString('hex');
-  const expires = Date.now() + 3600000; // 1時間
+    // トークン生成
+    const token = crypto.randomBytes(32).toString('hex');
+    const expires = Date.now() + 3600000; // 1時間
 
-  await requestPasswordResetRepository.updateResetToken(user._id, token, expires);
+    await requestPasswordResetRepository.updateResetToken(user._id, token, expires);
 
-  // tokenをSMTPサーバーに送る
-  const resetLink = `http://localhost:3000/home/login/password/reset/confirm?token=${token}`;
-  await sendResetEmail.sendTestResetEmail(user.email, resetLink);
+    // tokenをSMTPサーバーに送る
+    const resetLink = `http://localhost:3000/home/login/password/reset/confirm?token=${token}`;
+    await sendResetEmail.sendTestResetEmail(user.email, resetLink);
+  } catch (err) {
+    throw err;
+  }
 };
 
 module.exports = {
