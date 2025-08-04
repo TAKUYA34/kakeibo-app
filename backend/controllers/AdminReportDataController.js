6028b686e3233f25e1ae40505675b838223a7242
takuya-Y <hakenad907@gmail.com>
2025-08-04 16:17:37 +0900

    修正

--- backend/controllers/AdminReportDataController.js
+++ backend/controllers/AdminReportDataController.js
@@ -16,7 +16,7 @@ const getPaginatedAllNotices = async (req, res) => {
 /* 新規投稿したデータをDBに保存する */
 const createNotice = async (req, res) => {
   try {
-    const userId = req.user?._id ?? 'admin';
+    const userId = req.user?._id ?? req.user?.id ?? 'admin';
     const newNotice = await adminReportDataService.registerNotice({
       ...req.body,
       user_id: userId
@@ -36,6 +36,7 @@ const updateNotice = async (req, res) => {
     const updatedNotice = await adminReportDataService.updateNotice(req.params.id, req.body); // 動的にidを送る
     res.json(updatedNotice);
   } catch (err) {
+    // console.error(err.message);
     res.status(500).json({ message: 'お知らせの更新に失敗しました' });
   }
 };
@@ -46,6 +47,7 @@ const deleteNotice = async (req, res) => {
     await adminReportDataService.removeNotice(req.params.id); // 動的にidを送る
     res.status(204).send();
   } catch (err) {
+    // console.error(err.message);
     res.status(500).json({ message: 'お知らせの削除に失敗しました' });
   }
 };
