b5f94f90dbdc3159637754999fc3563d4dc61f6b
takuya-Y <hakenad907@gmail.com>
2025-08-05 22:48:19 +0900

    本番環境に向けて修正

--- backend/services/infoPagesFormService.js
+++ backend/services/infoPagesFormService.js
@@ -1,33 +1,41 @@
 const nodemailer = require('nodemailer');
+const path = require('path');
+const dotenv = require('dotenv');
 
-/* メールサーバー宛に送信する処理 */
+// 本番環境 or 開発環境
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config({ path: path.resolve(__dirname, '.env.development') });
+} else {
+  dotenv.config({ path: path.resolve(__dirname, '.env.production') });
+}
+
+/* Gmail宛に送信する処理 */
 const sendContactEmailService = async ({ name, email, message, subject }) => {
-//   const transporter = nodemailer.createTransport({
-//     host: 'smtp.ethereal.email',
-//     port: 587,
-//     auth: {
-//       user: process.env.ETHEREAL_USER,
-//       pass: process.env.ETHEREAL_PASS,
-//     },
-//   });
-
-  // 一時的なアカウントを自動生成
-  const testAccount = await nodemailer.createTestAccount();
-
-  // etherealサーバーにアクセス
   const transporter = nodemailer.createTransport({
-    host: 'smtp.ethereal.email',
-    port: 587,
+    service: 'gmail',
     auth: {
-      user: testAccount.user,
-      pass: testAccount.pass,
+      user: process.env.GMAIL_USER,
+      pass: process.env.GMAIL_PASS
     },
   });
+
+  // // 開発用アカウントを自動生成
+  // const testAccount = await nodemailer.createTestAccount();
+
+  // // etherealサーバーにアクセス
+  // const transporter = nodemailer.createTransport({
+  //   host: 'smtp.ethereal.email',
+  //   port: 587,
+  //   auth: {
+  //     user: testAccount.user,
+  //     pass: testAccount.pass,
+  //   },
+  // });
 
   // メールの形式
   const mailOptions = {
     from: `"${name}" <${email}>`,
-    to: process.env.ETHEREAL_USER,
+    to: process.env.GMAIL_USER,
     subject: `【Kakeibo-App お問い合わせ】【${subject}】 ${name} 様より`,
     text: `【お名前】
     ${name}
@@ -39,7 +47,8 @@ const sendContactEmailService = async ({ name, email, message, subject }) => {
 
   // 送信
   const info = await transporter.sendMail(mailOptions);
-  return nodemailer.getTestMessageUrl(info); // 表示リンクを返す（ログ用）
+  return info.messageId;
+  // return nodemailer.getTestMessageUrl(info); // 表示リンクを返す（ログ用）
 };
 
 module.exports = {
--- backend/utils/mailer.js
+++ backend/utils/mailer.js
@@ -1,18 +1,36 @@
 /* mailer.js */
 const nodemailer = require('nodemailer'); // メール生成用パッケージ
+const path = require('path');
+const dotenv = require('dotenv');
+
+// 本番環境 or 開発環境
+if (process.env.NODE_ENV !== 'production') {
+  dotenv.config({ path: path.resolve(__dirname, '.env.development') });
+} else {
+  dotenv.config({ path: path.resolve(__dirname, '.env.production') });
+}
 
 /* 指定したemailにリセットメールを送信する */
 const sendTestResetEmail = async (toEmail, resetLink) => {
   try {
-    const testAccount = await nodemailer.createTestAccount();
+    /* テスト用 */
+    // const testAccount = await nodemailer.createTestAccount();
 
     const transporter = nodemailer.createTransport({
-      host: 'smtp.ethereal.email',
-      port: 587,
+      /* 本番用 */
+      service: 'gmail',
       auth: {
-        user: testAccount.user,
-        pass: testAccount.pass
-      }
+        user: process.env.GMAIL_USER,
+        pass: process.env.GMAIL_PASS
+      },
+
+      /* テスト用 */
+      // host: 'smtp.ethereal.email',
+      // port: 587,
+      // auth: {
+      //   user: testAccount.user,
+      //   pass: testAccount.pass
+      // }
     });
 
     // この形式でメールを送信する
