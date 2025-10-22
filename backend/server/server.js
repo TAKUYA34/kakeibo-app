ed547a1154a4e88ec8c1e5cf1583232ba1c7ec50
takuya-Y <hakenad907@gmail.com>
2025-10-22 16:27:17 +0900

    開発用にDB接続先を変更した

--- backend/server/server.js
+++ backend/server/server.js
@@ -4,10 +4,13 @@ const mongoose = require('mongoose');
 const app = require('./app');
 
 const PORT = process.env.BACKEND_PORT || 5001;
+
 // docker設定用
 // const mongoUri = process.env.MONGO_URI || 'mongodb://mongo:27017/production_db';
+  const mongoUri = 'mongodb://host.docker.internal:27017/my_database';
+
 // local設定用
-const mongoUri = process.env.LOCAL_MONGO_URI || 'mongodb://localhost:27017/production_db';
+// const mongoUri = process.env.LOCAL_MONGO_URI || 'mongodb://localhost:27017/production_db';
 
 /* MongoDB接続 */
 mongoose.connect(mongoUri).then(() => {
@@ -15,7 +18,7 @@ mongoose.connect(mongoUri).then(() => {
 
     /* サーバー起動 */
   app.listen(PORT, () => {
-    // console.log(`サーバーがポート ${PORT} で起動しました`);
+    console.log(`サーバーがポート ${PORT} で起動しました`);
   });
 }).catch((err) => {
   console.error('MongoDB 接続エラー:', err);
